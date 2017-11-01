fa272388959cd286ce5b80fb8c0eecef276e53d9
zhiyun <zhiyun@m516-HP-EliteDesk-800-G1-TWR>
2017-11-01 13:20:31 +0800

    commment

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
+
+
+
+#try
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
